cfdabb721f25b3cf2eafa83986462e951a98060a
Milad E. Fahmy <milad.ez.fahmy@gmail.com>
2019-12-06 00:36:13 +0200

    Front End

--- routes/userPublickRouter.js
+++ routes/userPublickRouter.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const { user_signup, user_login } = require('../controllers/userController');
-router.all('*');
+router.get('/', (req, res) => {
+    res.sendFile('../dist/index.html', { root: __dirname });
+});
 router.post('/register', user_signup);
 router.post('/login', user_login);
 
